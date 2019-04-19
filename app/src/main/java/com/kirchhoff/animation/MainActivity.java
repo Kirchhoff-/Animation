05116b141781a029e8d1d0c2d6dfd8bbbcb9714d
Kirchhoff- <kirchhoff.19.92@gmail.com>
2019-04-19 21:22:32 +0300

    Rewrite TickerActivity to Kotlin.

--- app/src/main/java/com/kirchhoff/animation/MainActivity.java
+++ app/src/main/java/com/kirchhoff/animation/MainActivity.java
@@ -22,98 +22,99 @@ import com.kirchhoff.animation.ticker.TickerActivity;
  */
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
 
-    /**
-     * Ticker library
-     * https://github.com/robinhood/ticker
-     */
-    public void ticker(View view) {
-        TickerActivity.start(this);
-    }
+	/**
+	 * Ticker library
+	 * https://github.com/robinhood/ticker
+	 */
+	public void ticker(View view) {
+		Intent intent = new Intent(this, TickerActivity.class);
+		startActivity(intent);
+	}
 
-    /**
-     * Testing different android interpolators
-     */
-    public void interpolators(View view) {
-        InterpolatorActivity.start(this);
-    }
+	/**
+	 * Testing different android interpolators
+	 */
+	public void interpolators(View view) {
+		InterpolatorActivity.start(this);
+	}
 
-    /**
-     * Library for creating splash screen
-     * <p>
-     * https://github.com/ViksaaSkool/AwesomeSplash
-     */
+	/**
+	 * Library for creating splash screen
+	 * <p>
+	 * https://github.com/ViksaaSkool/AwesomeSplash
+	 */
 
-    public void awesomeSplash(View view) {
-        Intent intent = new Intent(this, AwesomeSplashActivity.class);
-        startActivity(intent);
-    }
+	public void awesomeSplash(View view) {
+		Intent intent = new Intent(this, AwesomeSplashActivity.class);
+		startActivity(intent);
+	}
 
-    /**
-     * Library for creating Particle view(view with interesting animation)
-     * <p>
-     * https://github.com/JeasonWong/Particle
-     */
-    public void particleView(View view) {
-        Intent intent = new Intent(this, ParticleActivity.class);
-        startActivity(intent);
-    }
+	/**
+	 * Library for creating Particle view(view with interesting animation)
+	 * <p>
+	 * https://github.com/JeasonWong/Particle
+	 */
+	public void particleView(View view) {
+		Intent intent = new Intent(this, ParticleActivity.class);
+		startActivity(intent);
+	}
 
-    /**
-     * https://github.com/Tapadoo/Alerter
-     */
-    public void alerter(View view) {
-        Intent intent = new Intent(this, AlerterActivity.class);
-        startActivity(intent);
-    }
+	/**
+	 * https://github.com/Tapadoo/Alerter
+	 */
+	public void alerter(View view) {
+		Intent intent = new Intent(this, AlerterActivity.class);
+		startActivity(intent);
+	}
 
-    /**
-     * https://github.com/bravoborja/FocusResize
-     */
-    public void focusResize(View view) {
-        FocusResizeActivity.start(this);
-    }
+	/**
+	 * https://github.com/bravoborja/FocusResize
+	 */
+	public void focusResize(View view) {
+		FocusResizeActivity.start(this);
+	}
 
-    /**
-     * https://github.com/StevenDXC/DxLoadingButton
-     */
-    public void loadingButton(View view) {
-        Intent intent = new Intent(this, LoadingButtonActivity.class);
-        startActivity(intent);
-    }
-
-
-    public void swipeBackLayout(View view) {
-        Intent intent = new Intent(this, SwipeBackLayoutActivity.class);
-        startActivity(intent);
-    }
-
-    /**
-     * https://github.com/iammert/ExpandableLayout
-     */
-    public void expandableLinearLayout(View view) {
-        Intent intent = new Intent(this, ExpandableLinearLayoutActivity.class);
-        startActivity(intent);
-    }
+	/**
+	 * https://github.com/StevenDXC/DxLoadingButton
+	 */
+	public void loadingButton(View view) {
+		Intent intent = new Intent(this, LoadingButtonActivity.class);
+		startActivity(intent);
+	}
 
 
-    /**
-     * https://github.com/xmuSistone/VegaLayoutManager
-     */
-    public void vegaLayoutManager(View view) {
-        VegaManagerActivity.start(this);
-    }
+	public void swipeBackLayout(View view) {
+		Intent intent = new Intent(this, SwipeBackLayoutActivity.class);
+		startActivity(intent);
+	}
 
-    /**
-     * https://github.com/iammert/ScalingLayout
-     */
-    public void scalingLayoutExample(View view) {
-        Intent intent = new Intent(this, ScalingMainActivity.class);
-        startActivity(intent);
-    }
+	/**
+	 * https://github.com/iammert/ExpandableLayout
+	 */
+	public void expandableLinearLayout(View view) {
+		Intent intent = new Intent(this, ExpandableLinearLayoutActivity.class);
+		startActivity(intent);
+	}
+
+
+	/**
+	 * https://github.com/xmuSistone/VegaLayoutManager
+	 */
+	public void vegaLayoutManager(View view) {
+		VegaManagerActivity.start(this);
+	}
+
+	/**
+	 * https://github.com/iammert/ScalingLayout
+	 */
+	public void scalingLayoutExample(View view) {
+		Intent intent = new Intent(this, ScalingMainActivity.class);
+		startActivity(intent);
+	}
 }
---
+++ app/src/main/java/com/kirchhoff/animation/ticker/TickerActivity.kt
@@ -0,0 +1,75 @@
+package com.kirchhoff.animation.ticker
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.view.animation.AnticipateInterpolator
+import android.view.animation.BounceInterpolator
+import android.view.animation.OvershootInterpolator
+import com.kirchhoff.animation.R
+import com.robinhood.ticker.TickerUtils
+import com.robinhood.ticker.TickerView
+import java.util.*
+
+/**
+ * @author Kirchhoff-
+ */
+class TickerActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var tickerView1: TickerView
+    private lateinit var tickerView2: TickerView
+    private lateinit var tickerView3: TickerView
+
+    private var charList = charArrayOf('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z')
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.ticker_activity)
+
+        tickerView1 = findViewById(R.id.tickerView1)
+        tickerView2 = findViewById(R.id.tickerView2)
+        tickerView3 = findViewById(R.id.tickerView3)
+
+        tickerView1.setOnClickListener(this)
+        tickerView2.setOnClickListener(this)
+        tickerView3.setOnClickListener(this)
+
+        tickerView1.setCharacterLists(TickerUtils.provideNumberList())
+        tickerView2.setCharacterLists(TickerUtils.provideAlphabeticalList())
+        tickerView3.setCharacterLists(String(charList))
+
+        tickerView1.text = randomIntNumber()
+        tickerView2.text = randomDoubleNumber()
+        tickerView3.text = randomString()
+
+        tickerView1.animationDuration = 1000
+        tickerView2.animationDuration = 2000
+        tickerView3.animationDuration = 3000
+
+        tickerView1.animationInterpolator = OvershootInterpolator()
+        tickerView2.animationInterpolator = BounceInterpolator()
+        tickerView3.animationInterpolator = AnticipateInterpolator()
+    }
+
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.tickerView1 -> tickerView1.text = randomIntNumber()
+            R.id.tickerView2 -> tickerView2.text = randomDoubleNumber()
+            R.id.tickerView3 -> tickerView3.text = randomString()
+        }
+    }
+
+    private fun randomIntNumber() = Random().nextInt(1000).toString()
+
+    private fun randomDoubleNumber() = Random().nextDouble().toString()
+
+    private fun randomString(): String {
+        val rand = Random()
+        val chars = "abcdefghijklmnopqrstuvwxyz"
+        val buf = StringBuilder()
+        for (i in 0..9) {
+            buf.append(chars[rand.nextInt(chars.length)])
+        }
+        return buf.toString()
+    }
+}

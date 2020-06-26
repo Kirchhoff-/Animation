4fae9dc5fbfea8edcfe0297a5fb6546ef29b1ab2
Kirchhoff- <kirchhoff.19.92@gmail.com>
2020-06-26 17:01:59 +0300

    Issue-35: Added example of usage Switcher library.

---
+++ switcher/src/main/java/com/kirchhoff/switcher/SwitcherActivity.kt
@@ -0,0 +1,8 @@
+package com.kirchhoff.switcher
+
+import androidx.appcompat.app.AppCompatActivity
+import com.example.switcher.R
+
+class SwitcherActivity : AppCompatActivity(R.layout.a_switcher) {
+
+}
